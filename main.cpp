8df170251822359ca1ff6b5d0eb44b964c707707
Nerijus Gabalis <ner147896523@gmail.com>
2019-02-07 11:06:10 +0200

    Added an exclamation mark on message end and gender identification.

--- main.cpp
+++ main.cpp
@@ -6,17 +6,24 @@ int main() {
     std::string name;
     std::cout << "Iveskite varda: ";
     std::cin >> name;
-    std::string greeting = "Sveikas, "+name;
-    for(int i = 0; i < greeting.length()+4; i++)
+    std::string greeting = "Sveika, "+name+"!";
+
+
+    if(name.back() == 's'){
+        greeting = "Sveikas, "+name+"!";
+    }
+
+
+    for(int i = 0; i < greeting.size()+4; i++)
         std::cout << "*";
     std::cout << std::endl << "* ";
-    for(int i = 0; i < greeting.length(); i++)
+    for(int i = 0; i < greeting.size(); i++)
         std::cout << " ";
     std::cout << " *" << std::endl << "* " << greeting << " *" << std::endl << "* ";
-    for(int i = 0; i < greeting.length(); i++)
+    for(int i = 0; i < greeting.size(); i++)
         std::cout << " ";
     std::cout << " *" << std::endl;
-    for(int i = 0; i < greeting.length()+4; i++)
+    for(int i = 0; i < greeting.size()+4; i++)
         std::cout << "*";
 
 
