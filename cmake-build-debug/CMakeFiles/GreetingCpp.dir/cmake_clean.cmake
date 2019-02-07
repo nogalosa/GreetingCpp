a5e79c3b149e7c6405a5c49aac42d85c5905ba5b
Nerijus Gabalis <ner147896523@gmail.com>
2019-02-07 10:31:02 +0200

    Init

---
+++ cmake-build-debug/CMakeFiles/GreetingCpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GreetingCpp.dir/main.cpp.obj"
+  "GreetingCpp.pdb"
+  "GreetingCpp.exe"
+  "GreetingCpp.exe.manifest"
+  "GreetingCpp.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GreetingCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
