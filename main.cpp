ae6edcd75208b7f4666ea2df91d6b9099074c42e
Nerijus Gabalis <ner147896523@gmail.com>
2019-02-07 11:21:27 +0200

    Code clean up and added a border size setting.

--- main.cpp
+++ main.cpp
@@ -6,26 +6,42 @@ int main() {
     std::string name;
     std::cout << "Iveskite varda: ";
     std::cin >> name;
+
+    int borderSize = 0;
+    std::cout << "Iveskite remelio stori:";
+    std::cin >> borderSize;
+
+
     std::string greeting = "Sveika, "+name+"!";
-
-
     if(name.back() == 's'){
         greeting = "Sveikas, "+name+"!";
     }
 
 
-    for(int i = 0; i < greeting.size()+4; i++)
-        std::cout << "*";
-    std::cout << std::endl << "* ";
-    for(int i = 0; i < greeting.size(); i++)
-        std::cout << " ";
-    std::cout << " *" << std::endl << "* " << greeting << " *" << std::endl << "* ";
-    for(int i = 0; i < greeting.size(); i++)
-        std::cout << " ";
-    std::cout << " *" << std::endl;
-    for(int i = 0; i < greeting.size()+4; i++)
-        std::cout << "*";
+    std::string firstLine = "";
+    for(int i = 0; i < greeting.size() + 2 + borderSize * 2; i++)
+        firstLine += "*";
 
+    std::string secondLine = "*";
+    for(int i = 0; i < greeting.size() + borderSize * 2; i++)
+        secondLine += " ";
+    secondLine += "*";
+
+    std::string thirdLine = "*";
+    for(int i = 0; i < borderSize; i++)
+        thirdLine += " ";
+    thirdLine += greeting;
+    for(int i = 0; i < borderSize; i++)
+        thirdLine += " ";
+    thirdLine += "*";
+
+    std::cout << firstLine << std::endl;
+    for(int i = 0; i < borderSize; i++)
+        std::cout << secondLine << std::endl;
+    std::cout << thirdLine << std::endl;
+    for(int i = 0; i < borderSize; i++)
+        std::cout << secondLine << std::endl;
+    std::cout << firstLine << std::endl;
 
     return 0;
 }
